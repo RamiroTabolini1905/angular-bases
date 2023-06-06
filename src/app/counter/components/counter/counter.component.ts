650b9c06400d276a0f335d67c88e32aaa454db38
Ramiro Tabolini <ramirotabolini@gmail.com>
2023-06-06 00:12:12 -0300

    Fin de seccion - 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <p>Counter: {{counter}}</p>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `,
+})
+export class CounterComponent {
+  public title:string = 'Hola Mundo';
+  public counter:number = 10;
+
+  constructor(){}
+  increaseBy(value:number):void{
+    this.counter += value;
+  }
+  resetCounter():void{
+    this.counter = 10;
+  }
+}
